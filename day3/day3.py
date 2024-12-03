38e273641708325b2c344309d93345a0dd220a4a
Ed <em1258@stern.nyu.edu>
2024-12-03 14:28:52 +0000

    day3 crushes

--- day3/day3.py
+++ day3/day3.py
@@ -9,14 +9,13 @@ def do_mult(instruction):
 
 with open("day3.in","r") as f:
     total=0
-    lines = f.read()
+    lines = f.read().replace('\n','')
     pattern = r"(mul\([0-9]+,[0-9]+\))"
     ignore = r"don't\(\)(.+?)do\(\)"
     ignore_eol = r"don't\(\)(.+?)$"
     line = lines
     line = re.sub(ignore,"", line)
     line = re.sub(ignore_eol,"", line)
-    print(line)
     matches = re.findall(pattern,line)
     matches = map(lambda x: do_mult(x), matches)
     total += sum(matches)
