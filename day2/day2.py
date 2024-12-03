efbb692edaa1db6a214a115176bca0d8d8f990a3
Ed <ed.markovich@gmail.com>
2024-12-03 01:16:14 -0500

    day2.2 done

--- day2/day2.py
+++ day2/day2.py
@@ -1,8 +1,16 @@
-def safeReport(report):
+def safeReport_helper(report):
     lvl_diffs = [abs(report[i] - report[i-1]) for i in range (1, len(report))]
     if min(lvl_diffs) < 1 or max(lvl_diffs)>3: return False
     return report == sorted(report) or report == sorted(report, reverse=True)
 
+def safeReport(report):
+    if safeReport_helper(report): return True
+
+    for i in range(len(report)):
+        subReport = report[:i] + report[i+1:]
+        if safeReport_helper(subReport): return True
+
+    return False
 
 with open("day2.in","r") as f:
     lines = f.readlines()
