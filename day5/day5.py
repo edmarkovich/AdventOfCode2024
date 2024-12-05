21023d56c5e43620cc950f78d5f0707e68804e4e
Ed <em1258@stern.nyu.edu>
2024-12-05 06:21:44 +0000

    day5 crushed

--- day5/day5.py
+++ day5/day5.py
@@ -5,13 +5,23 @@ lines = open("day5.in").readlines()
 rules = [x.strip().split("|") for x in lines if "|" in x]
 pages = [x.strip().split(",") for x in lines if "," in x]
 
-def checkRulePattern(rule, pattern):
+def checkRulePattern(rule, pattern, fix=False):
 	if rule[0] not in pattern or rule[1] not in pattern: return True
-	return pattern.index(rule[0]) < pattern.index(rule[1])
+	if pattern.index(rule[0]) < pattern.index(rule[1]): return True
+
+	if fix:
+		el = pattern.pop(pattern.index(rule[0]))
+		pattern.insert(pattern.index(rule[1]),el)
+
+	return False
 
 out = 0
 for p in pages:
-	if False in [checkRulePattern(r,p) for r in rules]: continue
+	if not False in [checkRulePattern(r,p) for r in rules]: continue
+	passes=0
+	while False in [checkRulePattern(r,p, True) for r in rules]: 
+		passes += 1
+		pass
 
 	idx = math.floor(len(p)/2)
 	out += int(p[idx])
