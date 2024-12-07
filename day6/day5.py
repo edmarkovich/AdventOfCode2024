6d957e1c2b517206064dcf137bcd608d967f9116
Ed <em1258@stern.nyu.edu>
2024-12-07 03:55:12 +0000

    made terser

--- day6/day5.py
+++ day6/day5.py
@@ -6,62 +6,35 @@ obs = [(i, j) for i in range(dim[0]) for j in range(dim[1]) if grid[i][j] == "#"
 
 
 def destinationFromObstacle(orient, obstacles, current, dim_max):
-	if orient in ["UP", "LEFT"]:
-		extreme = max
-		filt = lambda x: x< current
-		offset = +1
-		exit_max = 0
-	else:
-		extreme = min
-		filt = lambda x: x>current
-		offset = -1
-		exit_max = dim_max-1
+	less = orient in ["UP", "LEFT"]
+
+	extreme = max if less else min
+	filt = (lambda x: x< current) if less else (lambda x: x>current)
+	offset = +1 if less else -1
+	exit_max = 0 if less else dim_max
 
 	obstacles = [o for o in obstacles if filt(o)]
-	if obstacles:
-		return extreme(obstacles) + offset
-
-	return exit_max
-
+	return (extreme(obstacles) + offset, False) if obstacles else (exit_max, True)
 
 def nextLoc(row, col, orient):
-	exited = False
-	if orient in ["LEFT", "RIGHT"]:
-		obstacles = [c[1] for c in obs if c[0] == row]
-		filt= col
-		exit_max = dim[1]
-	else: 
-		obstacles = [r[0] for r in obs if r[1] == col]
-		filt = row
-		exit_max = dim[0]
+	horizontal = orient in ["LEFT", "RIGHT"]
 
-	destination = destinationFromObstacle(orient, obstacles, filt, exit_max)	
-	if destination in [0, exit_max-1]:
-		exited = True
-				
-	if orient in ["LEFT", "RIGHT"]:
-		return (row, destination, abs(destination-col), exited)
-	else:
-		return (destination, col, abs(destination-row), exited)
-		
+	obstacles = [c[1] for c in obs if c[0] == row] if horizontal else [r[0] for r in obs if r[1] == col]
 
+	destination, exited = destinationFromObstacle(orient, obstacles, 
+		col if horizontal else row,
+		dim[1]-1 if horizontal else dim[1]-1)			
 
-moves = 1
+	return (row, destination, exited) if horizontal else (destination, col, exited)
+
 visited = set()
 for orientation in ["UP","RIGHT","DOWN","LEFT"]*100000:
-	(r,c,m,exited) = nextLoc(row,col,orientation)
-	moves +=m
-	#print(r,c,m,moves,exited)
-	
-	for i in range(min(r,row),max(r,row)+1):
-		for j in range(min(c,col),max(c,col)+1):
-			visited.add((i,j))
-
-	row = r
-	col = c	
-
-
+	r,c,exited = nextLoc(row,col,orientation)
+	visited.update((i,j) 
+		for i in range(min(r,row),max(r,row)+1) 
+		for j in range(min(c,col),max(c,col)+1))
 	if exited: break
-print(moves)
+	row,col = r,c
+
 print(len(visited))
 
