f56b72ad8d3e9115b95affb9af9d3c2195e3aada
Ed <em1258@stern.nyu.edu>
2024-12-08 06:38:51 +0000

    day 7.1

---
+++ day7/day7.py
@@ -0,0 +1,19 @@
+from itertools import product
+
+def parseLine(line):
+	first,rest = line.split(":")
+	return int(first), [int(x) for x in rest.split()]
+
+def permute(target, ints):
+	for operations in [p for p in product("+*", repeat=len(ints)-1)]:
+		total = ints[0]
+		for o in range(len(operations)):
+			if operations[o]=="+": total += ints[1+o]
+			if operations[o]=="*": total *= ints[1+o]
+		if total == target: return True
+	return False
+
+out = 0
+for line in [parseLine(x) for x in open("day7.in").readlines()]:
+	if permute(line[0], line[1]): out += line[0]
+print(out)
