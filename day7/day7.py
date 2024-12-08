c0c68f448f36ee96a4922c4062772c7331ea274d
Ed <em1258@stern.nyu.edu>
2024-12-08 07:12:34 +0000

    tweaks

--- day7/day7.py
+++ day7/day7.py
@@ -1,23 +1,23 @@
 from itertools import product
+import operator
+
+ops = [operator.add, operator.mul, lambda x,y: int(str(x)+(str(y)))]
 
 def parseLine(line):
 	first,rest = line.split(":")
 	return int(first), [int(x) for x in rest.split()]
 
 def permute(target, ints):
-	for operations in [p for p in product("+*|", repeat=len(ints)-1)]:
+	for operations in [p for p in product(ops, repeat=len(ints)-1)]:
 		total = ints[0]
 		for o in range(len(operations)):
-			if operations[o]=="+": total += ints[1+o]
-			elif operations[o]=="*": total *= ints[1+o]
-			elif operations[o]=="|": total = int(str(total)+str(ints[1+o]))
+			total=operations[o](total, ints[1+o])
 		if total == target: return True
 	return False
 
-out = 0
-count=0
-for line in [parseLine(x) for x in open("day7.in").readlines()]:
-	count +=1
-	print(count)
-	if permute(line[0], line[1]): out += line[0]
-print(out)
+def do():
+	lines = [parseLine(x) for x in open("day7.in").readlines()]
+	out = sum([line[0] for line in lines if permute(line[0], line[1])])
+	print(out)
+
+do()
